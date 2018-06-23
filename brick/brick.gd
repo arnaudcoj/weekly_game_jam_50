33c81eac13cc747844716b33e1d8d005003a9c47
arnaudcoj <arnaudcoj@protonmail.com>
2018-06-23 17:19:55 +0200

    add dummy brick ia

--- brick/brick.gd
+++ brick/brick.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 
-export var speed = 500
+export var speed = 800
 export var human = false
 
 var velocity = Vector2()
 
 func _ready():
-	pass
+	if !human:
+		$Timer.wait_time = rand_range(.2, 5.0)
+		$Timer.start()
 
 func _process(delta):
 	if human:
@@ -14,7 +16,6 @@ func _process(delta):
 	
 func human_control():
 	if Input.is_action_just_pressed("ui_right"):
-		
 		apply_impulse(Vector2(0,0), Vector2(speed,0))
 	if Input.is_action_just_pressed("ui_left"):
 		apply_impulse(Vector2(0,0), Vector2(-speed,0))
@@ -22,3 +23,19 @@ func human_control():
 		apply_impulse(Vector2(0,0), Vector2(0,-speed))
 	if Input.is_action_just_pressed("ui_down"):
 		apply_impulse(Vector2(0,0), Vector2(0,speed))
+		
+func _on_Timer_timeout():
+	if !human:
+		var dir_choice = randi() % 4
+		
+		if dir_choice == 0 :
+			apply_impulse(Vector2(0,0), Vector2(speed,0))
+		elif dir_choice == 1 :
+			apply_impulse(Vector2(0,0), Vector2(-speed,0))
+		elif dir_choice == 2 :
+			apply_impulse(Vector2(0,0), Vector2(0,-speed))
+		elif dir_choice == 3 :
+			apply_impulse(Vector2(0,0), Vector2(0,speed))
+		
+		$Timer.wait_time = rand_range(1.0, 5.0)
+		$Timer.start()
