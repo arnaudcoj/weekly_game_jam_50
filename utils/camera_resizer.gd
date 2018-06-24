884dafba1aab77676baeb62bfc80677fdb4afe98
arnaudcoj <arnaudcoj@protonmail.com>
2018-06-24 16:45:56 +0200

    add camera resizing according to the window size

---
+++ utils/camera_resizer.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+var max_zoom = 5
+
+onready var default_res = Vector2(ProjectSettings.get_setting("display/window/size/width"),ProjectSettings.get_setting("display/window/size/height"))
+
+func _ready():
+	print(default_res)
+	get_tree().get_root().connect("size_changed",self,"on_resize")
+	
+func on_resize():
+	var zoom = max(default_res.y/get_viewport_rect().size.y, default_res.x / get_viewport_rect().size.x)
+	zoom = min(zoom, max_zoom)
+
+	set_zoom(Vector2(zoom,zoom))
