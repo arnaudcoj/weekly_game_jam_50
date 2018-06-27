4262be87a514cf900a1c2ca2690ccbe733103eac
arnaudcoj <arnaudcoj@protonmail.com>
2018-06-27 16:44:37 +0200

    circle reduces by waves

--- circle/circle.gd
+++ circle/circle.gd
@@ -1,14 +1,31 @@
 extends StaticBody2D
 
-export var final_scale = 0.4
-export var scale_speed = 0.05
+export var scales = [[5, 1.2, 0.05], [5, .8, 0.05], [4, .5, 0.04], [2, .3, 0.02]] 
+var scale_i = 0
+
+var scaling = false
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	$Timer.wait_time = scales[scale_i][0]
+	$Timer.start()
+	scaling = false
 
 func _process(delta):
-	scale = scale - Vector2(scale_speed * delta, scale_speed * delta)
-	scale.x = max(final_scale, scale.x)
-	scale.y = max(final_scale, scale.y)
+	if scaling:
+		scale = scale - Vector2(1,1) * scales[scale_i][2] * delta
+		if scale.x <= scales[scale_i][1]:
+			stop_scaling()
+			
+func start_scaling():
+	scaling = true
+	
+func stop_scaling():
+	scaling = false
+	scale_i += 1
+	if scale_i < scales.size():
+		$Timer.wait_time = scales[scale_i][0]
+		$Timer.start()
+		
+
+func _on_Timer_timeout():
+	start_scaling()
