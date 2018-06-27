2f325c861e3e70aa7f72f26676f0f6bafb8f5db3
arnaudcoj <arnaudcoj@protonmail.com>
2018-06-27 16:11:29 +0200

    cpu ia uses dash loading

--- brick/random_strategy.gd
+++ brick/random_strategy.gd
@@ -2,21 +2,32 @@ extends Node
 
 onready var brick = get_parent()
 
+var loading = false
+
 func _ready():
 	$Timer.wait_time = rand_range(.2, 5.0)
 	$Timer.start()
+	loading = false
 
 func _on_Timer_timeout():
-	var dir_choice = randi() % 4
-	
-	if dir_choice == 0 :
-		brick.apply_impulse(Vector2(0,0), Vector2(brick.speed * rand_range(0.2,1.0),0))
-	elif dir_choice == 1 :
-		brick.apply_impulse(Vector2(0,0), Vector2(-brick.speed * rand_range(0.2,1.0),0))
-	elif dir_choice == 2 :
-		brick.apply_impulse(Vector2(0,0), Vector2(0,-brick.speed * rand_range(0.2,1.0)))
-	elif dir_choice == 3 :
-		brick.apply_impulse(Vector2(0,0), Vector2(0,brick.speed * rand_range(0.2,1.0)))
-	
-	$Timer.wait_time = rand_range(1.0, 5.0)
+	if loading:
+		brick.stop_dash_load()
+		$Timer.wait_time = rand_range(2.0, 5.0)
+		loading = false
+	else:
+		var dir_choice = randi() % 4
+		
+		if dir_choice == 0 :
+			brick.start_dash_load(Vector2(1,0))
+		elif dir_choice == 1 :
+			brick.start_dash_load(Vector2(-1,0))
+		elif dir_choice == 2 :
+			brick.start_dash_load(Vector2(0,-1))
+		elif dir_choice == 3 :
+			brick.start_dash_load(Vector2(0,1))
+		
+		$Timer.wait_time = rand_range(0.05, brick.max_load_time)
+		
+		loading = true
+		
 	$Timer.start()
